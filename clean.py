064280fb31a090e2b3526c9a72da311e2659c6e5
HungMingWu <u9089000@gmail.com>
2016-06-27 14:05:40 +0800

    Rewrite segmentfault's rip function

--- clean.py
+++ clean.py
@@ -15,50 +15,22 @@ def removeScript(soup):
 		script.decompose()
 	return soup
 
+def removeTag(soup, tagname, attribute_name, atribute_value_array):
+	for attribute_value in atribute_value_array:
+		for tag in soup.findAll(tagname, {attribute_name : attribute_value}):
+			tag.decompose()
+	return soup
+
 def removeSegmentFaultTag(soup):
-	div = soup.find("div", {"class": "col-md-4"})
-	div.decompose()
-	div = soup.find("div", {"class": "clearfix mt10"})
-	div.decompose()
-	div = soup.find("div", {"class": "widget-box"})
-	div.decompose()
-	div = soup.find("div", {"class": "recommend-post"})
-	div.decompose()
-	div = soup.find("div", {"class": "text-center mt10"})
-	div.decompose()
-	div = soup.find("div", {"class": "global-navTags"})
-	div.decompose()
-	div = soup.find("div", {"class": "post-topheader custom-"})
-	div.decompose()
-	div = soup.find("div", {"class": "global-nav sf-header"})
-	div.decompose()
-	div = soup.find("div", {"class": "app-promotion-bar"})
-	div.decompose()
-	div = soup.find("div", {"id": "fixedTools"})
-	div.decompose()
-	div = soup.find("div", {"class": "widget-comments hidden"})
-	div.decompose()
-	div = soup.find("div", {"class": "modal"})
-	div.decompose()
-	div = soup.find("div", {"class": "hidden widget-register widget-welcome-question mt20 hidden-xs widget-welcome widget-register-slideUp"})
-	div.decompose()
-	div = soup.find("div", {"class": "modal widget-911"})
-	div.decompose()
-	div = soup.find("div", {"class": "col-xs-12 col-md-3 side"})
-	div.decompose()
-	footer = soup.find("footer", {"id": "footer"})
-	footer.decompose()
-	img = soup.find('img', {'id' : 'icon4weChat'})
-	img.decompose()
-	img = soup.find('img', {'id' : 'icon4weChat'})
-	img.decompose()
-	discuss = soup.find('h2', {'class' : 'h4 post-comment-title'})
-	discuss.decompose()
-	for meta in soup.findAll('meta'):
-		if not meta.has_attr('charset'):
-			meta.decompose()
-	for meta in soup.findAll('link'):
-        	meta.decompose()
+	soup = removeTag(soup, "div", "class",
+			["col-md-4", "clearfix mt10", "widget-box", "recommend-post", "text-center mt10", "global-navTags",
+			"post-topheader custom-", "global-nav sf-header", "app-promotion-bar", "widget-comments hidden", "modal",
+			"hidden widget-register widget-welcome-question mt20 hidden-xs widget-welcome widget-register-slideUp",
+			"modal widget-911", "col-xs-12 col-md-3 side"])
+	soup = removeTag(soup, "img", "id", ['icon4weChat', 'icon4weChat'])
+	soup = removeTag(soup, "div", "id", ["fixedTools"])
+	soup = removeTag(soup, "footer", "id", ["footer"])
+	soup = removeTag(soup, "h2", "class", ['h4 post-comment-title'])
 	return soup
 
 def removeJobboleTag(soup):
@@ -172,6 +144,11 @@ def removeIBMTag(soup):
 	soup = removeTag(soup, "p", "class", ["ibm-ind-link ibm-back-to-top"])
 	return soup
 
+def removeFreeBufTag(soup):
+	soup = removeTag(soup, "div", "class",
+		["panel panel-default", "commentshow", "comment-list", "panel panel-default rec-spe"])
+	return soup
+
 if len(sys.argv) < 2:
     sys.stderr.write('Usage: clean [url] ')
     sys.exit(1)
@@ -190,6 +167,8 @@ elif "iteblog" in url:
 	soup = removeITEBlog(soup)
 elif "www.ibm.com/developerworks" in url:
 	soup = removeIBMTag(soup)
+elif "www.freebuf.com" in url:
+	soup = removeFreeBufTag(soup)
 
 html = soup.prettify("utf-8")
 
